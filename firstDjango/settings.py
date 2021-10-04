62d05f22d2d8da46854f08d3d7973894ff53078d
Mark Routledge <mark@mroutledge.co.uk>
2021-10-04 06:41:29 +0100

    Don't leave Secret Keys on GitHub

--- firstDjango/settings.py
+++ firstDjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3_@1um71_5zd3=gbydhhpql6g@#xzk@oz4@0r7m5mbob$l!b*k'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
